bba03a0f4a03042d58136683ad3c8433a83da32c
joedupor <joedupor@gmail.com>
2023-09-26 17:23:13 -0500

    finished exercise1

---
+++ ORM-Dapper/Department.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Dapper
+{
+    public class Department  // this represents the departments table of 
+    {
+
+        public int DepartmentID { get; set; }
+        public string Name { get; set; }
+
+
+    }
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace ORM_Dapper
+﻿using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace ORM_Dapper
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connString = config.GetConnectionString("DefaultConnection");
+
+            IDbConnection conn = new MySqlConnection(connString);
+
+            var departmentRepo = new DapperDepartmentRepository(conn);
+
+            departmentRepo.InsertDepartment("Xtra Large Electronics");
+
+            var departments = departmentRepo.GetAllDepartments();
+
+            foreach (var item in departments)
+            {
+                Console.WriteLine(item.DepartmentID);
+                Console.WriteLine(item.Name);
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+
         }
     }
 }
